5539ec673123846f94e2ee0f6db971f8c94a9659
Edward Wilson <ed@lighthouseandgiant.co.uk>
2014-11-04 08:08:25 +0000

    Added typekit support & WP Login logo

--- wp-content/themes/boilerplate/functions.php
+++ wp-content/themes/boilerplate/functions.php
@@ -97,11 +97,25 @@ function html5blank_header_scripts()
 
         wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
         wp_enqueue_script('modernizr'); // Enqueue it!
+        
+        wp_register_script('typekit', 'http://use.typekit.net/************.js'); // Typekit (Replace url)
+        wp_enqueue_script('typekit'); // Enqueue it!
 
         wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
     }
 }
+
+// TypeKit Fonts
+add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+
+function theme_typekit_inline() {
+  if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+  	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+}
+add_action( 'wp_head', 'theme_typekit_inline' );
+
 
 // Load HTML5 Blank conditional scripts
 function html5blank_conditional_scripts()
@@ -448,5 +462,11 @@ function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 short
 {
     return '<h2>' . $content . '</h2>';
 }
+// Custom WordPress Login Logo
+function login_css() {
+	wp_enqueue_style( 'login_css', get_template_directory_uri() . '/css/login.css' );
+}
+add_action('login_head', 'login_css');
+// Require Metaboxes
 require_once('cmb/metaboxes.php');
 ?>
